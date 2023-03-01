80b0ef767a7883d940e91a526538271e64c7b0c8
sunhongli <hongli.sun@gaodun.com>
2023-03-01 14:14:56 +0800

    专门展示自定义控件的页面：WidgetActivity

--- app/src/main/java/com/hl/sun/ui/activity/MainActivity.kt
+++ app/src/main/java/com/hl/sun/ui/activity/MainActivity.kt
@@ -7,7 +7,6 @@ import com.hl.sun.R
 import com.hl.sun.adapter.MainAdapter
 import com.hl.sun.bean.MainRvBean
 import com.hl.sun.ui.TouchScrollActivity
-import com.hl.weblib.core.util.FileUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         list.add(0, MainRvBean("动画展示页面", AnimActivity::class.java))
         list.add(0, MainRvBean("横向滚动条RecyclerView", RecyclerViewActivity::class.java))
         list.add(0, MainRvBean("H5静态资源缓存", WebCacheActivity::class.java))
+        list.add(0, MainRvBean("自定义控件", WidgetActivity::class.java))
 
         return list
     }
---
+++ app/src/main/java/com/hl/sun/ui/activity/WidgetActivity.kt
@@ -0,0 +1,18 @@
+package com.hl.sun.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hl.sun.R
+
+/**
+ * Function:自定义控件展示
+ * Date:2023/3/1
+ * Author: sunHL
+ */
+class WidgetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_widget)
+    }
+
+}
